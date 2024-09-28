8eb40220e0e012e52ac80044b87426d2e3c4cf5c
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2024-09-29 06:04:54 +0900

    group 및 sidebox 컴포넌트 수정

--- src/components/Group/GroupContents.jsx
+++ src/components/Group/GroupContents.jsx
@@ -4,11 +4,11 @@ const GroupContents = () => {
   return (
     <div className="flex flex-row basis-1/2 justify-around">
       {/* 이미지 부분 */}
-      <div className="">
-        <img className=" w-15 h-20" src="/book_example.png" alt="error" />
+      <div className="flex justify-center items-center">
+        <img className=" w-5/6 h-5/6" src="/book_example.png" alt="error" />
       </div>
       {/* 텍스트 부분 */}
-      <div className="">
+      <div className="flex flex-col justify-center items-start w-5/6 whitespace-nowrap">
         <div className=" text-lg">그룹 이름</div>
         <div className=" text-md">책 이름</div>
         <div className=" text-gray-500 text-sm">모임장명 | 3 / 3명</div>
