e571df2e9f46706bf99eea3be503191fc632e135
inqueue0979 <inqueue0979@gmail.com>
2024-09-29 04:08:41 +0900

    Changed Navbar Functioning

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BottomNav = () => {
   return (
     <div className="relative">
-        <div className="fixed z-50 h-16 -translate-x-1/2 bg-white bg-opacity-60 border border-gray-200 border-opacity-20 rounded-3xl bottom-4 left-1/2 backdrop-blur-2xl drop-shadow-md mx-auto">
+        <div className="fixed z-50 w-[70%] max-w-[380px] h-16 -translate-x-1/2 bg-white bg-opacity-60 border border-gray-200 border-opacity-20 rounded-3xl bottom-4 left-1/2 backdrop-blur-2xl drop-shadow-md mx-auto">
         <div className="grid h-full grid-cols-4 mx-auto">
             <a href="/" data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
                 <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -21,7 +21,7 @@ const BottomNav = () => {
                     <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
                     <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"/>
                 </svg>
-                <p className="text-sm text-gray-500">모임 찾기</p>
+                <p className="text-xs text-gray-500">모임 찾기</p>
             </a>
             <div id="tooltip-wallet" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-sm shadow-sm opacity-0 tooltip dark:bg-gray-700">
                 Wallet
@@ -35,7 +35,7 @@ const BottomNav = () => {
                 <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
                 </svg>
-                <p className="text-sm text-gray-500">모임 생성</p>
+                <p className="text-xs text-gray-500">모임 생성</p>
             </a>
             <div id="tooltip-settings" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-sm shadow-sm opacity-0 tooltip dark:bg-gray-700">
                 Settings
