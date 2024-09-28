4bcf524c2bbcd61784993856bb770e837bc737d5
haileybaileyo <dreamchick07>
2024-09-28 21:55:31 +0900

    bookcontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
+import './App.css';
+import RectangleWithText from './components/bookcontent'; 
 function App() {
-
   return (
     <div>
       <h1 className="text-3xl font-bold text-emerald-600">readwith</h1>
+      <RectangleWithText /> {/* RectangleWithText 컴포넌트 사용 */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
