00d19172785c1608cca1ac8be048397ce1e99bb6
inqueue0979 <inqueue0979@gmail.com>
2024-09-28 22:43:47 +0900

    Added Various Files

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div>
+        <h1 className="sulphur-point-regular">readwith</h1>
+    </div>
+  );
+};
+
+export default Topbar;
