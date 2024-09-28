3b6f0d77a93930984404bceea1f85daed31ad8e8
inqueue0979 <inqueue0979@gmail.com>
2024-09-28 15:28:45 +0900

    Added tailwindcss & postcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Hello cokothon!</h1>
-    </>
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">TailwindCSS가 작동합니다!</h1>
+    </div>
   )
 }
 
